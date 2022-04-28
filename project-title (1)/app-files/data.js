8008b46554cc112664ce61f110a5715e6c3afe00
LeeHwajeong <82190650+LeeHwajeong@users.noreply.github.com>
2022-04-29 03:24:35 +0900

    ss

---
+++ project-title (1)/app-files/data.js
@@ -0,0 +1,34 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kakaotalk_20220415_025913213",
+      "name": "KakaoTalk_20220415_025913213",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "yaw": -1.6667771631937658,
+        "pitch": 0.3793568925947728,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
